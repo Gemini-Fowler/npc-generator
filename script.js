6e8592ffe894c5e0b940d01d98dcb007ed57aaf5
Jonas Crothers <jonascrothers@gmail.com>
2025-10-02 20:20:29 -0700

    const expansions and addition of flaws

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.getElementById("generateBtn").addEventListener("click", () => {
-  const races = ["Elf", "Dwarf", "Human", "Orc"];
-  const classes = ["Wizard", "Rogue", "Cleric", "Fighter"];
+  const races = ["Elf", "Dwarf", "Human", "Orc", "Halfling", "Gnome", "Tiefling", "Dragonborn", "Half-Orc", "Half-Elf", "Aasimar", "Air Genasi", "Earth Genasi", "Fire Genasi", "Water Genasi", "Bugbear", "Goblin", "Hobgoblin", "Kobold", "Lizardfolk", "Tabaxi", "Triton", "Yuan-ti Pureblood"];
+  const classes = ["Wizard", "Rogue", "Cleric", "Fighter", "Ranger", "Paladin", "Bard", "Druid", "Monk", "Warlock", "Sorcerer", "Barbarian", "Artificer", "Blood Hunter", "Cavalier", "Samurai", "Gunslinger", "Mystic", "Warden", "Alchemist", "Archer", "Assassin", "Beastmaster", "Shadowdancer"];
   const traits = ["Optimist", "Cynic", "Introvert", "Extrovert"];
+  const flaws = ["Greedy", "Arrogant", "Cowardly", "Impulsive", "Stubborn", "Lazy", "Jealous", "Vengeful", "Pessimistic", "Hot-tempered", "Gullible", "Overconfident", "Secretive", "Reckless", "Dishonest", "Selfish", "Naive", "Paranoid", "Materialistic", "Indecisive", "Aloof", "Melancholic", "Sarcastic", "Skeptical", "Timid", "Untrustworthy"];
 
   const npc = {
     race: races[Math.floor(Math.random() * races.length)],
     class: classes[Math.floor(Math.random() * classes.length)],
-    trait: traits[Math.floor(Math.random() * traits.length)]
+    trait: traits[Math.floor(Math.random() * traits.length)],
+    flaw: flaws[Math.floor(Math.random() * flaws.length)]
   };
 
   document.getElementById("npcCard").innerHTML = `
